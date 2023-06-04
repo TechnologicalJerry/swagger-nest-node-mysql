783f69367024edc4cd080dafe821c36b7eb2113c
technologicaljerry <cserajatverma@gmail.com>
2023-06-04 18:52:22 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,10 +13,14 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      // type: 
-      // host: 
-      // port: 
-
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true
     })
   ],
   controllers: [AppController],
