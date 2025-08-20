1ed2aea0d9a473213f4b643fb8ff81d726eb7e28
TechnologicalJerry <cserajatverma@gmail.com>
2025-08-20 23:54:54 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-
-const PORT = 5000;
-const HOST = 'localhost';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('Users CRUD')
-    .setDescription('The Users API description')
-    .setVersion('1.0')
-    .addTag('user')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/swagger-ui', app, document);
-
-  await app.listen(PORT);
-  Logger.log(`SERVER RUNNING ON HOST=> ${HOST} AT PORT=> ${PORT}`);
+  await app.listen(process.env.PORT ?? 3000);
 }
-
 bootstrap();
