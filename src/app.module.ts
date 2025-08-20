bfd5937cf78b886640efe9dc714963c7a91ea46c
TechnologicalJerry <cserajatverma@gmail.com>
2025-08-20 23:55:08 +0530

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DATABASE_HOST,
-      port: 3306,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      entities: [],
-      synchronize: true,
-    }),
-    UsersModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
